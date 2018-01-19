ca22993c80006529f306f8c21f5a392405b02428
zacksleo <zacksleo@gmail.com>
2018-01-19 17:48:10 +0800

    test:

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,4 +3,4 @@ define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 require_once __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-Yii::setAlias('@tests', __DIR__ . '/api');
+Yii::setAlias('@tests', __DIR__);
