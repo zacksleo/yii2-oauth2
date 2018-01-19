771ba6f96521e69fa826460f7226319d0f07c378
zacksleo <zacksleo@gmail.com>
2018-01-19 16:36:38 +0800

    tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-require_once '../vendor/autoload.php';
-require '../vendor/yiisoft/yii2/Yii.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
