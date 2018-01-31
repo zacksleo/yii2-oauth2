ef29a9f669b61305c5435545e98b11b8415cc3d4
zacksleo <zacksleo@gmail.com>
2018-01-31 10:58:09 +0800

    revert(api): 恢复默认跨域允许

--- src/api/components/RestController.php
+++ src/api/components/RestController.php
@@ -36,6 +36,9 @@ class RestController extends Controller
         ]);
         $auth = $behaviors['authenticator'];
         unset($behaviors['authenticator']);
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+        ];
         $behaviors['authenticator'] = $auth;
         $behaviors['authenticator']['except'] = ['options'];
         return $behaviors;
