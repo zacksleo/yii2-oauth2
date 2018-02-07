b35e5238b4b75946c182ad66cfbcd1eb2beac3c7
zacksleo <zacksleo@gmail.com>
2018-02-07 16:27:11 +0800

    fix(api): 跨域时, 客户端拿不到分页信息

--- src/api/components/RestController.php
+++ src/api/components/RestController.php
@@ -38,6 +38,19 @@ class RestController extends Controller
         unset($behaviors['authenticator']);
         $behaviors['corsFilter'] = [
             'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => null,
+                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Expose-Headers' => [
+                    'X-Pagination-Current-Page',
+                    'X-Pagination-Page-Count',
+                    'X-Pagination-Per-Page',
+                    'X-Pagination-Total-Count'
+                ],
+            ],
         ];
         $behaviors['authenticator'] = $auth;
         $behaviors['authenticator']['except'] = ['options'];
